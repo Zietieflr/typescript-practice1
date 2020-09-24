358352bfd8e97a92312b33fb995e7648b016cdef
Zietieflr <lrmcguire93@gmail.com>
2020-09-24 08:18:03 -0600

    Complete first practice with Typescript.

---
+++ index.ts
@@ -0,0 +1,9 @@
+// not always the best practice, but good to practice with
+const isAwesome: boolean = true;
+
+function sum(number1: number, number2: number): number {
+  return number1 + number2
+}
+
+sum(1, 2)
+sum(1, "Fred")
